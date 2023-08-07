af1cfd87d9fd0e7745067f13acae4d1f916548b0
Ryota Suzuki <suzuki@ef-prime.com>
2023-08-07 18:52:11 +0900

    add tests with refactoring

--- R/internal.R
+++ R/internal.R
@@ -5,7 +5,8 @@
   readLines(con)
 }
 
-.get_pkg_by_date <- function(url_str) {
+.get_pkg_by_date <- function(repos) {
+  url_str <- paste0(repos, "/web/packages/available_packages_by_date.html")
   html <- .get_html(url_str)
 
   # get table rows from html.
@@ -29,8 +30,7 @@
   pkg_available <- as.data.frame(
     utils::available.packages(repos = repos, type = "source"))
 
-  url <- paste0(repos, "/web/packages/available_packages_by_date.html")
-  pkg_by_date <- .get_pkg_by_date(url)
+  pkg_by_date <- .get_pkg_by_date(repos)
 
   merge(
     pkg_by_date,
@@ -52,6 +52,32 @@
 
     return(d_flat)
   }
+}
+
+.get_archive_url <- function(repos) {
+  paste0(repos, "/src/contrib/Archive/")
+}
+
+.get_archive_url_package <- function(repos, package) {
+  paste0(.get_archive_url(repos), package, "/")
+}
+
+.get_df_archive <- function(repos, package) {
+  url_str <- .get_archive_url_package(repos, package)
+  try_result <- try(txt <- .get_html(url_str))
+
+  if(inherits(try_result, "try-error")) {
+    stop("Package '", package, "' is not available at ", repos)
+  }
+
+  rows <- txt[grepl("<a href=.*\\d{4}-\\d{2}-\\d{2}", txt)]
+  df <- data.frame(
+    File = sub('^ *<a href="(.+\\.tar\\.gz)">.*$', '\\1', rows),
+    Date = sub('^.*(\\d{4}-\\d{2}-\\d{2}).*$', '\\1', rows),
+    stringsAsFactors = FALSE
+  )
+
+  return(df)
 }
 
 .collect <- function(
@@ -94,20 +120,9 @@
         gzfile_date <- date_latest
       } else {
         status <- "archive"
-        url <- paste0(repos, "/src/contrib/Archive/", p, "/")
 
-        try_result <- try(txt <- .get_html(url))
-
-        if(inherits(try_result, "try-error")) {
-          stop("Package '", p, "' is not available at ", repos)
-        }
-
-        rows <- txt[grepl("<a href=.*\\d{4}-\\d{2}-\\d{2}", txt)]
-        df <- data.frame(
-          File = sub('^ *<a href="(.+\\.tar\\.gz)">.*$', '\\1', rows),
-          Date = sub('^.*(\\d{4}-\\d{2}-\\d{2}).*$', '\\1', rows),
-          stringsAsFactors = FALSE
-        )
+        # get data.frame of archived versions
+        df <- .get_df_archive(repos, p)
 
         df_download <- local({
           tmp <- df[df$Date <= date, , drop = FALSE]
@@ -116,7 +131,7 @@
         })
 
         gzfile_name <- df_download$File
-        gzfile_url <- paste0(url, gzfile_name)
+        gzfile_url <- paste0(.get_archive_url_package(repos, p), gzfile_name)
         gzfile_date <- df_download$Date
       }
 
---
+++ tests/tests.R
@@ -0,0 +1,24 @@
+repos <- "https://cloud.r-project.org"
+
+# get package list by date
+pkg_by_date <- dateback:::.get_pkg_by_date(repos)
+head(pkg_by_date)
+
+# get latest package list
+pkg_latest <- dateback:::.get_pkg_latest(repos)
+head(pkg_latest)
+
+# get archived version list
+.get_archived_pkgs <- function(repos) {
+  html <- dateback:::.get_html(dateback:::.get_archive_url(repos))
+  rows <- grep("<a href=\".*?/\">.*?/",
+               strsplit(
+                 paste(html, collapse = " "), # some rows may contain \n
+                 "</a>")[[1]],
+               value = TRUE)
+  sub(".*<a href=\".*?/\">(.*?)/.*", "\\1", rows)
+}
+
+archived_pkgs <- .get_archived_pkgs(repos)
+df_archive <- dateback:::.get_df_archive(repos, sample(archived_pkgs, 1))
+head(df_archive)
