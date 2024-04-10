1dc845dca6f029e474d778fc4651e50f5a62894d
Ryota Suzuki <suzuki@ef-prime.com>
2024-04-10 18:22:32 +0900

    Fix to handle newlines in DESCRIPTION file, etc. (#8)
    * fix for non-utf8 environment
    
    * fix for "pkg\n(>= x.y.z)" in DESCRIPTION
    
    * Update DESCRIPTION

--- R/internal.R
+++ R/internal.R
@@ -1,5 +1,5 @@
 .get_html <- function(url_str) {
-  con <- base::url(url_str)
+  con <- base::url(url_str, encoding = "UTF-8")
   on.exit(close(con))
 
   readLines(con)
@@ -45,7 +45,9 @@
     return(character(0))
   } else {
     d_flat <- local({
-      tmp <- unlist(strsplit(d[!is.na(d) & d != ""], ","), recursive = TRUE)
+      # TODO: maybe it can be simpler, but be careful with marginal cases
+      tmp <- unlist(strsplit(d[!is.na(d) & d != ""], ",\\s*"), recursive = TRUE)
+      tmp <- sub("\\s", " ", tmp) # pkg\n(>= x.y.z) existed
       tmp <- trimws(tmp)
       sub("^([^ (]+).*$", "\\1", tmp) # "pkg (>= x.y.z)" is expected, but "pkg(>= x.y.z)" exists
     })
